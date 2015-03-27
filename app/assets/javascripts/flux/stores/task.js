76da9d33d1a6921d80b0266570380406d3a4cc0f
alecguintu <aeguintu@gmail.com>
2015-03-27 15:24:58 +0800

    Small refactor to Store all.

--- app/assets/javascripts/flux/stores/task.js
+++ app/assets/javascripts/flux/stores/task.js
@@ -1,14 +1,20 @@
 modulejs.define('taskStore', ['underscore', 'jquery', 'eventEmitter'], function(_, $, EventEmitter) {
 
   var _tasks = Immutable.OrderedMap();
+  
+  // Private methods
+  function create(params) {
+    _tasks = _tasks.set(params.id, Immutable.fromJS(params))
+  }
+
+  // Public methods
 
   var TaskStore = {
     all: function() {
-      var taskData = $.parseJSON($("#tasks-data").html())
-
-      _.each(taskData, function(task) {
-        _tasks = _tasks.set(task.id, Immutable.fromJS(task))
-      })
+      // If initially empty, check inside the html given by rails' controller
+      if (_tasks.size == 0) {
+        _.each($.parseJSON($("#tasks-data").html()), function(task) { create(task) })
+      }
 
       return _tasks;
     },
